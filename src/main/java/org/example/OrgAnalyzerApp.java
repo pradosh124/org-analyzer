1abfcd9a03c37baebdf5844f2ee6de6d47ff1a42
pradoshchakra <pradoshchakra10@gmail.com>
2025-10-01 22:02:00 +0530

    added test cases and code changes1

--- src/main/java/org/example/OrgAnalyzerApp.java
+++ src/main/java/org/example/OrgAnalyzerApp.java
@@ -12,7 +12,7 @@ import java.util.Map;
 public class OrgAnalyzerApp {
     public static void main(String[] args) throws IOException {
         OrgAnalyzer analyzer = new OrgAnalyzer();
-        analyzer.loadEmployees("src/main/resources/employees1.csv");
+        analyzer.loadEmployees("src/main/resources/employees.csv");
 
         List<OrgRule> rules = List.of(new SalaryRule(), new ReportingLineRule());
         Map<String, List<String>> issues = analyzer.runRules(rules);
